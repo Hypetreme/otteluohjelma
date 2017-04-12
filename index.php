9b245b7c450cbad1dc6fe5ef5d91ae24a45e0dcf
Janne Karppinen <hypetremethewanderer@gmail.com>
2017-04-12 12:30:09 +0300

    bugeja korjattu, katsojanäkymää päivitetty ja palvelun etusivu lisätty

--- index.php
+++ index.php
@@ -1,69 +1,263 @@
+<html>
 <?php
-  include('inc/header.php');
-  session_start();
-  if (isset($_SESSION['id'])) {
-      header("Location: profile.php");
-  }
+include('head.php');
 ?>
-
-<!-- Tähän sitten se switchi -->
-
-<div class="container">
-  <span id="msg" class="msg-fail"></span>
-  <div class="row">
-    <div class="twelve columns">
-      <h1>
-        Otteluohjelma
-      </h1>
-    </div>
-  </div>
-  <div class="row">
-    <form id="form" action="functions.php" method="POST">
-      <div class="twelve columns">
-        <h4>
-          Kirjaudu sisään
-        </h4>
-      </div>
-      <div class="twelve columns">
-        <label for="uid">
-          Käyttäjänimi
-        </label>
-        <input class="styled" type="text" id="uid">
-      </div>
-      <div class="twelve columns">
-        <label for="pwd">
-          Salasana
-        </label>
-        <input type="password" id="pwd">
-      </div>
-      <div class="twelve columns">
-        <input class="button-primary" id="logIn" id="submit" type="submit" value="Kirjaudu">
-        <input class="button-primary" type="button" value="Rekisteröidy" onclick="window.location.href='register.php'">
-      </div>
-    </form>
-
-
-  </div>
-
-  <div class="row">
-    <div class="twelve columns">
-    </div>
-  </div>
-
-
+<body>
+<a name="0"/></a>
+<div class="up" onclick="scrollToAnchor('0')"><p>YLÖS</p></div>
+<div class="main">
+<div class="hamburger"><i class="material-icons">storage</i></div>
+<div class="hamburger-items">
+<div onclick="scrollToAnchor('1')"><p>Tietoa</p></div>
+<div onclick="scrollToAnchor('2')"><p>Palvelu</p></div>
+<div onclick="scrollToAnchor('3')"><p>Esimerkkejä</p></div>
+<div onclick="scrollToAnchor('4')"><p>Yhteystiedot</p></div>
+<a href="./login/index.php"><div><p>Kirjaudu</p></div></a>
 </div>
-<script>
-$('form').submit(function(event){
-    event.preventDefault(); // stop the form from submitting
-    var username = $('#uid').val();
-    var pass = $('#pwd').val();
-    var finish = $.post("functions.php", { logIn: 'login', pwd: pass, uid: username }, function(data) {
-      if(data){
-        console.log(data);
-      }
-      message(data);
+<div class="main-nav">
+<div class="main-outer"></div>
+<div class="main-inner">
+<div><p onclick="scrollToAnchor('1')">Tietoa</p></div>
+<div><p onclick="scrollToAnchor('2')">Palvelu</p></div>
+<div><p onclick="scrollToAnchor('3')">Esimerkkejä</p></div>
+<div><p onclick="scrollToAnchor('4')">Yhteystiedot</p></div>
+<a href="./login/index.php"><div><p>Kirjaudu</p></div></a>
+</div>
+<div class="main-outer"></div>
+</div>
+<div class="main-header">
+<p>OTTELUOHJELMA</p>
+</div>
+</div>
 
-    });
+<div class="twelve columns main-area">
+  <a name="1"/>
+  <div class="main-area-header">
+  <h1>OTSIKKO</h1>
+  </div>
+
+<div class="text-column">
+<div class="four columns">
+<div class="main-content-header">
+<p>Otsikko</p>
+</div>
+<div class="main-area-content">
+<p>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+</p>
+</div>
+
+<div class="main-content-header">
+<p>Otsikko</p>
+</div>
+<div class="main-area-content">
+<p>
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+</div>
+</div>
+
+<div class="four columns">
+<div class="main-content-header">
+<p>Otsikko</p>
+</div>
+<div class="main-area-content">
+<p>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+</p>
+</div>
+
+<div class="main-content-header">
+<p>Otsikko</p>
+</div>
+<div class="main-area-content">
+<p>
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+</div>
+</div>
+
+<div class="four columns">
+<div class="main-content-header">
+<p>Otsikko</p>
+</div>
+<div class="main-area-content">
+<p>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+</p>
+</div>
+
+<div class="main-content-header">
+<p>Otsikko</p>
+</div>
+<div class="main-area-content">
+<p>
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+</div>
+</div>
+</div>
+</div>
+<div class="twelve columns main-bottom">
+  <a name="2"/>
+  <div class="bottom-area-header">
+  <h1>OTSIKKO</h1>
+  </div>
+  <div class="text-column">
+
+  <div class="twelve columns">
+  <div class="main-content-header">
+  <p>Otsikko</p>
+  </div>
+  <div class="main-area-content">
+  <p>
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  </p>
+  <p>
+  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </p>
+  <p>
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  </p>
+  </div>
+
+  </div>
+
+  </div>
+</div>
+<div class="twelve columns main-end">
+  <a name="3"/>
+  <div class="main-area-header">
+  <h1>OTSIKKO</h1>
+  </div>
+
+<div class="text-column">
+<div class="four columns">
+<div class="main-content-header">
+<p>Otsikko</p>
+</div>
+<div class="main-area-content">
+<p>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+</p>
+</div>
+
+<div class="main-content-header">
+<p>Otsikko</p>
+</div>
+<div class="main-area-content">
+<p>
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+</div>
+</div>
+
+<div class="four columns">
+<div class="main-content-header">
+<p>Otsikko</p>
+</div>
+<div class="main-area-content">
+<p>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+</p>
+</div>
+
+<div class="main-content-header">
+<p>Otsikko</p>
+</div>
+<div class="main-area-content">
+<p>
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+</div>
+</div>
+
+<div class="four columns">
+<div class="main-content-header">
+<p>Otsikko</p>
+</div>
+<div class="main-area-content">
+<p>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+</p>
+</div>
+
+<div class="main-content-header">
+<p>Otsikko</p>
+</div>
+<div class="main-area-content">
+<p>
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+</div>
+</div>
+</div>
+</div>
+
+<div class="footer">
+<a name="4"/>
+<div class="footer-cell">
+<div class="footer-box first"></div>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+</div>
+<div class="footer-cell">
+<div class="footer-box second"></div>
+<p>Appstudios Finland Oy 2017<br>
+Höyläämötie 3B<br>
+00380<br>
+HELSINKI</p>
+</div>
+<div class="footer-cell">
+<div class="footer-box third"></div>
+<p>Otteluohjelma.fi</p>
+</div>
+<div class="footer-cell">
+<div class="footer-box fourth"></div>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+</div>
+</div>
+</body>
+</html>
+<script>
+
+$( ".hamburger" ).click(function(event) {
+  event.stopPropagation();
+  $( ".hamburger" ).toggleClass('open');
+  if ($( ".hamburger" ).hasClass('open')) {
+  $( ".hamburger-items" ).slideDown();
+  $( ".hamburger-items" ).css('display', 'inline-block');
+} else {
+  $( ".hamburger-items" ).slideUp();
+}
 });
+
+$(window).click(function() {
+    if ($( ".hamburger" ).hasClass('open')) {
+    $( ".hamburger-items" ).slideUp();
+    $( ".hamburger" ).toggleClass('open');
+  }
+});
+
+function scrollToAnchor(link){
+    var aTag = $("a[name='"+ link +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},1000);
+}
 </script>
-<?php include('inc/footer.php'); ?>
